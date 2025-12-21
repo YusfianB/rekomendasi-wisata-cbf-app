cf018a6c2ee0d093d5df59b211e6f3873c8d4bdb
lintingggg <imuh6500@gmail.com>
2025-12-22 00:40:41 +0700

    buat pages

--- pages/Business_Understanding.py
+++ pages/Business_Understanding.py
@@ -1,13 +1,8 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Business Understanding",
+    page_icon="💼"
+)
+
 st.title("Business Understanding")
-
-col1, col2 = st.columns(2)
-
-with col1:
-    st.subheader("Kolom Kiri")
-    st.write("Isi kolom pertama")
-
-with col2:
-    st.subheader("Kolom Kanan")
-    st.write("Isi kolom kedua")
---
+++ pages/Data_Preparation.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Data Preparation",
+    page_icon="💼"
+)
+
+st.title("Data Preparation")
